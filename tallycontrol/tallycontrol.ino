1a1f6f8154bf3c40dd6f086141775553e7ddfa0f
Jamason Davis <jpdavis2471@gmail.com>
2024-12-11 18:02:34 -0600

    Basic empty sketch.

---
+++ tallycontrol/tallycontrol.ino
@@ -0,0 +1,14 @@
+#include <BMDSDIControl.h>
+
+const int shieldAddress = 0x6E;
+BMD_SDITallyControl_I2C sdTallyControl(shieldAddress);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
