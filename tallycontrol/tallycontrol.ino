1af6a19917927397e205350a3ece20543d516259
Jamason Davis <jpdavis2471@gmail.com>
2024-12-12 10:49:31 -0600

    Added working tally program.

--- tallycontrol/tallycontrol.ino
+++ tallycontrol/tallycontrol.ino
@@ -1,14 +1,61 @@
+// Library Includes
 #include <BMDSDIControl.h>
 
+// Define our  outward tally pins.
+#define CAM1 6
+#define CAM2 7
+
+// BMD Shield i2c address and class init.
 const int shieldAddress = 0x6E;
-BMD_SDITallyControl_I2C sdTallyControl(shieldAddress);
+BMD_SDITallyControl_I2C sdiTallyControl(shieldAddress);
 
+// Create our storage variables, for tally signals and for the debug output.
+bool cam1_prog, cam1_prev;
+bool cam2_prog, cam2_prev;
+String out;
+
+// One time setup() function.
 void setup() {
-  // put your setup code here, to run once:
+  // Open our debug serial port.
+  Serial.begin(9600);
+  
+  sdiTallyControl.begin();
+  sdiTallyControl.setOverride(true);
 
+  pinMode(CAM1, OUTPUT);
+  pinMode(CAM2, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+bool checkDebug(int num) {
+  if(Serial.available()) {
+    char receivedChar = Serial.read();
+    if(receivedChar == num) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
+//  This is pretty rudimentary programming, but we only have 2 cameras with tally support. Not much reason to change no?
+void loop() {
+  out = "";
+  out.concat("Cam 1: ");
+  if(sdiTallyControl.getCameraTally(1, cam1_prog, cam2_prev)) {
+    if(cam1_prog) {
+      digitalWrite(CAM1, HIGH);
+      out.concat("ON    Cam 2: ");
+    } else {
+      out.concat("OFF   Cam 2: ");
+    }
+  }
+  if(sdiTallyControl.getCameraTally(2, cam2_prog, cam2_prev)) {
+    if(cam2_prog) {
+     digitalWrite(CAM2, HIGH);
+      out.concat("ON\r");
+    } else {
+      out.concat("OFF\r");
+    }
+  }
+  Serial.println(out);
 }
